c3a31f333deaa52adb7d43f3e0b39dc7f7595705
Enrico <enrico.sandrini@allievi.bearzi.it>
2023-12-04 18:11:40 +0100

    Prima bozza css e sondaggio v1

---
+++ sondaggio.php
@@ -0,0 +1,37 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en"> <!-- TO-DO Vedere della lingua -->
+    <head>
+        <link rel="stylesheet" href="main.css">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <div class="mainDiv">
+            <h1>Domanda</h1>
+            <div>
+                <div class="selectDiv">
+                    <h2>SASSO</h2>
+                </div>
+                <div class="selectDiv">
+                    <h2>SASSO</h2>
+                </div>
+                <div class="selectDiv">
+                    <h2>SASSO</h2>
+                </div>
+                <div class="selectDiv">
+                    <h2>SASSO</h2>
+                </div>
+            </div>
+            <div>
+                <div class="selectDiv">
+                    <h2>NEXT</h2>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
